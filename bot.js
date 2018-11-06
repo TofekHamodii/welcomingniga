1cedc6c322734f204bc5d25b7f8ad0f13ea3a0e7
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-06 19:05:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('guildMemberAdd' , member => {
 const room = client.channel.get("508838387449593879")
-room.send("**Welcome To # -  Universe Town " :champagne_glass: ** "+member)
+room.send("**Welcome To # -  Universe Town , :champagne_glass: ** "+member)
 });
 
 client.login(process.env.BOT_TOKEN);
