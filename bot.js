c816ccf62214d6211e7cdb8587fa9199acec6923
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-07 19:21:38 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("# -  Universe Town ",{type: 'WATCHING'});
+  
+  });
+  
+client.login("NTA3OTc4Mjg4MjUwMDkzNTY4.DsHdrQ.iegZjEFqryYKyt2jdTqut0l0OC0");
